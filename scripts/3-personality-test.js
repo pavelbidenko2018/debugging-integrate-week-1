b1ff70cd530cd721fe327a454064473b0a04d5f6
Pavel Bidenko <pavelbidenko2018@gmail.com>
2020-06-14 20:58:36 +0200

    blanks fiiled

--- scripts/3-personality-test.js
+++ scripts/3-personality-test.js
@@ -3,36 +3,41 @@
 console.log('--- loading: personality test');
 
 function personalityTestHandler() {
-  'use strict';
-  debugger;
+    'use strict';
+    debugger;
 
-  // read inputs from user
-  const likesPuppies = alert(`Do you like puppies?`);
-  const eatsVegetables = alert(`Do you eat vegetables?`);
-  const sleepsEnough = alert('Do you sleep enough?');
+    // read inputs from user
+    const likesPuppies = alert(`Do you like puppies?`);
+    const eatsVegetables = alert(`Do you eat vegetables?`);
+    const sleepsEnough = alert('Do you sleep enough?');
 
-  // before moving on ... type check!
-  if (typeof likesPuppies !== 'Boolean') { throw new TypeError(); }
-  if (typeof eatsVegetables !== 'Boolean') { throw new TypeError(); }
-  if (typeof sleepsEnough !== 'Boolean') { throw new TypeError(); }
+    console.log(typeof likesPuppies);
 
-  // perform core logic
-  const isHealthy = eatsVegetables || sleepsEnough;
-  const isAGoodPerson = likesPuppies && isHeathy;
-  const adjective = isAGoodPerson ? 'good' : 'bad';
-  const message = `You are a ``${adjective}`` person`;
+    // before moving on ... type check!
+    if (typeof likesPuppies !== 'Boolean') { throw new TypeError(); }
+    if (typeof eatsVegetables !== 'Boolean') { throw new TypeError(); }
+    if (typeof sleepsEnough !== 'Boolean') { throw new TypeError(); }
 
-  // alert result for the user
-  alert(message);
+    // perform core logic
+    const isHealthy = eatsVegetables || sleepsEnough;
+    const isAGoodPerson = likesPuppies && isHeathy;
+    const adjective = isAGoodPerson ? 'good' : 'bad';
+    const message = `You are a `
+    `${adjective}`
+    ` person`;
 
-  // log action for the developer
-  console.log('\n--- personality test ---');
-  console.log('likesPuppies:', typeOf likesPuppies, '\n', likesPuppies);
-  console.log('eatsVegetables:', typeOf eatsVegetables, '\n', eatsVegetables);
-  console.log('sleepsEnough:', typeOf sleepsEnough, '\n', sleepsEnough);
-  console.log('isHealthy:', typeOf isHealthy, '\n', isHealthy);
-  console.log('isAGoodPerson:', typeOf isAGoodPerson, '\n', isAGoodPerson);
-  console.log('adjective:', typeOf adjective, '\n', adjective);
-  console.log('message:', typeOf message, '\n', message);
+    // alert result for the user
+    alert(message);
+
+    // log action for the developer
+    console.log('\n--- personality test ---');
+    console.log('likesPuppies:', typeof likesPuppies, '\n', likesPuppies);
+    console.log('likesPuppies:', typeof likesPuppies, '\n', likesPuppies);
+    console.log('eatsVegetables:', typeof eatsVegetables, '\n', eatsVegetables);
+    console.log('sleepsEnough:', typeof sleepsEnough, '\n', sleepsEnough);
+    console.log('isHealthy:', typeof isHealthy, '\n', isHealthy);
+    console.log('isAGoodPerson:', typeof isAGoodPerson, '\n', isAGoodPerson);
+    console.log('adjective:', typeof adjective, '\n', adjective);
+    console.log('message:', typeof message, '\n', message);
 
 }
